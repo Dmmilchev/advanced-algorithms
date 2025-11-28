7824d8b8a0e64bde8ffe4e5f506f1ae4ce86d5ea
Dmmilchev <deo1508@abv.bg>
2025-11-28 18:56:48 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # advanced-algorithms
+
+* [Level Ancestor](./LA) is the problem of answering queries of the type: **for a node in a tree v and a positive number k, which is the ancestor of v on depth k in the tree**. There are efficient algorithms that create an index (data structure) which can efficiently answer such queries. I have implemented the algorithm which can be found in the [The Level Ancestor Problem simplified paper](./LA/LA.pdf).
+
+* [Range Minimum Query](./RMQ) is the problem of answering queries of the type: **given a list of numbers L and indices in the list i, j, return the minimum of the subarray L[i, ..., j]**. In the [cpp](./RMQ) I have implemented an index structure that answers such queries efficiently when the list L satisfy the following property: for each i, abs(L[i] - L[i+1]) <= 1. This is also known as +-1RMQ and it has an interesting [connection](./RMQ/LCA.pdf) with another algorithmic problem called lowest common ancestor (LCA).
+
+* [String Perfect Hash](./SPH) is the problem of creating a perfect hash that works for strings. The hash function must be computable in time linear with respect to the input string. In the [cpp](./SPH/SPH.cpp) I have implemented a data structure (automaton) that is created in time linear with respect to the static dictionary which then enables us to compute a hash function with the properties specified. 
+
+* [Wagner-Fischer algorithm](./WFA) is considered by some the first dynamic programming algorithm. You can check the [implementation](./WHA/WHA.cpp) and the [paper](https://par.cse.nsysu.edu.tw/resource/lab_relative/Wagn74.pdf)
--- SPH/SPH.cpp
+++ SPH/SPH.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 #include <algorithm>
 #include <random>
+#include <cassert>
 
 #define INVALID UINT64_MAX
 
@@ -23,13 +24,17 @@ struct Func {
 };
 
 struct Trie {
-    std::string alphabet = "zyxwvutsrqponmlkjihgfedcba";
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
     size_t last;
-    std::unordered_set<size_t> final;
+    std::unordered_set<size_t> is_final;
     Func delta;
     Func lambda;
+    std::vector<size_t> Nq;
 
-    Trie() = default;
+    Trie() {
+        last = 0;
+        Nq = std::vector<size_t>({0});
+    }
 
     void addWord(const std::string& word) {
         size_t q = 0;
@@ -39,14 +44,23 @@ struct Trie {
             if (delta.get(q, word[i]) != INVALID) q = delta.get(q, word[i]);
             else break;
         }
+        if (i == word.size()) { is_final.insert(q); }
 
         for(i; i < word.size(); ++i) {
             ++last;
             delta.set(q, word[i], last);
             q = last;
+            Nq.push_back(0);
         }
 
-        final.insert(last);
+        size_t v = 0;
+        Nq[v]++;
+        for (char c : word) {
+            v = delta.get(v, c);
+            Nq[v]++;
+        }
+
+        is_final.insert(last);
     }
 
     bool traverse(const std::string& word) {
@@ -56,27 +70,36 @@ struct Trie {
 
             if (q == INVALID) return false;
         }
-        return final.count(q) > 0;
+        return is_final.count(q) > 0;
     }
 
     size_t hash(const std::string& word) {
         size_t q = 0;
         size_t hash = 0;
         for (char c : word) {
-            q = delta.get(q, c);
-            if (q == INVALID) return INVALID;
+            size_t v = delta.get(q, c);
+            if (v == INVALID) return INVALID;
             hash += lambda.get(q, c);
+            q = v;
         }
         return hash;
     }
 
     void initLambdaRec(size_t q) {
-        size_t k = final.count(q);
         for (char c: alphabet) {
             size_t v = delta.get(q, c);
+
             if (v != INVALID) {
-                lambda.set(q, c, k);
-                ++k;
+                size_t x = is_final.count(q);
+
+                for (char c2 : alphabet) {
+                    if (c2 >= c) break;
+                    size_t v2 = delta.get(q, c2);
+                    if (v2 == INVALID) continue;
+                    x += Nq[v2];
+                }
+                lambda.set(q, c, x);
+
                 initLambdaRec(v);
             }
         }
@@ -90,10 +113,9 @@ struct Trie {
 std::vector<std::string> generateRandomStrings(int N) {
     std::vector<std::string> result;
     
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    std::mt19937 gen(123);
     std::uniform_int_distribution<> lengthDist(5, 15);  // String length between 5-15
-    std::uniform_int_distribution<> charDist(0, 25);    // For a-z
+    std::uniform_int_distribution<> charDist(0, 20);
     
     for (int i = 0; i < N; ++i) {
         std::string randomStr;
@@ -106,22 +128,38 @@ std::vector<std::string> generateRandomStrings(int N) {
         result.push_back(randomStr);
     }
     
+    std::unordered_set<std::string> seen;
+    std::vector<std::string> unique;
+
+    unique.reserve(result.size());
+
+    for (const auto& s : result) {
+        if (seen.insert(s).second) {
+            unique.push_back(s);
+        }
+    }
+
+    result = std::move(unique);
     return result;
 };
 
 
 int main() {
-    std::vector<std::string> strings = generateRandomStrings(10);
+    std::vector<std::string> strings = generateRandomStrings(100000);
     Trie t;
     for (auto s : strings) {
         t.addWord(s);
     }
-    std::sort(strings.begin(), strings.end());
-    for (auto s : strings) {
-        std::cout << t.traverse(s) << std::endl;
-        std::cout << t.hash(s) << std::endl;;
+    t.initLambda();
 
+    std::sort(strings.begin(), strings.end());
+    size_t h = 0;
+    for (auto s : strings) {
+        assert(h == t.hash(s));
+        h++;
     }
-    std::cout << strings[0];
+
+
+
     return 0;
 }
